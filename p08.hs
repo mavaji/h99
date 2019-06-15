bdbf12547465d6cce71ce4f5b7cbfe6e036ff607
Vahid Mavaji <mavaji@outlook.com>
2019-06-15 18:06:39 +0430

    problem 8

---
+++ p08.hs
@@ -0,0 +1,10 @@
+main :: IO ()
+main = do
+  print(compress "aaaabccaadeeee")
+
+compress :: (Eq a) => [a] -> [a]
+compress [] = []
+compress [x] = [x]
+compress (x:xs)
+  | x == head(xs) = compress xs
+  | otherwise = x:compress xs
