dbf36d5bf92d19ec70ae96880b528978ec8698d6
Vahid Mavaji <mavaji@outlook.com>
2019-06-15 15:49:53 +0430

    problem 6

---
+++ p06.hs
@@ -0,0 +1,10 @@
+main :: IO ()
+main = do
+  print(isPalindrome [1,2,3])
+  print(isPalindrome "madamimadam")
+  print(isPalindrome [1,2,4,8,16,8,4,2,1])
+
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome [] = True
+isPalindrome [x] = True
+isPalindrome xs = (head xs == last xs) && isPalindrome ((init.tail) xs)
