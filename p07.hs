658d07634f2759a9770da25be6b1da47beab31ff
Vahid Mavaji <mavaji@outlook.com>
2019-06-15 17:55:19 +0430

    problem 7

---
+++ p07.hs
@@ -0,0 +1,12 @@
+main :: IO ()
+main = do
+  print(flatten (Elem 5))
+  print(flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]]))
+--  print(flatten (List []))
+
+data NestedList a = Elem a | List [NestedList a] deriving Show
+
+flatten :: NestedList a -> [a]
+flatten (List []) = []
+flatten (Elem x) = [x]
+flatten (List (x:xs)) = flatten x ++  flatten(List xs)
