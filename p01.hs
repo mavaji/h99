0e909c661d04af8b87a28ac8acf097afeab146b6
Vahid Mavaji <mavaji@outlook.com>
2019-06-15 15:17:32 +0430

    problem 1

---
+++ p01.hs
@@ -0,0 +1,10 @@
+main :: IO()
+main = do
+  print(myLast [1,2,3,4])
+  print(myLast ['x', 'y', 'z'])
+
+
+myLast :: [a] -> a
+myLast [] = error "empty list"
+myLast [x] = x
+myLast (x:xs) = myLast xs
