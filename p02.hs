725b0008e0d4ac5fcf026e06e893164f62514897
Vahid Mavaji <mavaji@outlook.com>
2019-06-15 15:26:55 +0430

    problem 2

---
+++ p02.hs
@@ -0,0 +1,10 @@
+main :: IO ()
+main = do
+  print(myButLast [1,2,3,4])
+  print(myButLast ['a'..'z'])
+
+myButLast :: [a] -> a
+myButLast [] = error "empty list"
+myButLast [x] = error "one element list"
+myButLast [x, y] = x
+myButLast (x:xs) = myButLast xs
