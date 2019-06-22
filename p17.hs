3d8064101c16b11c17de829cc49c8755a75c35b2
Vahid Mavaji <mavaji@outlook.com>
2019-06-22 15:10:56 +0430

    problem 17

---
+++ p17.hs
@@ -0,0 +1,11 @@
+-- Split a list into two parts; the length of the first part is given.
+-- Do not use any predefined predicates.
+main :: IO ()
+main = do
+  print $ split "abcdefghik" 3
+
+split :: [a] -> Int -> ([a], [a])
+split [] _ = ([], [])
+split (x:xs) 1 = ([x], xs)
+split (x:xs) i = (x:fst split_i_1, snd split_i_1)
+  where split_i_1 = split xs (i - 1)
