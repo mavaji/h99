141291d61a54250f1afefc9b29b3ac7bf8ed2526
Vahid Mavaji <mavaji@outlook.com>
2019-06-23 13:45:32 +0430

    problem 32

---
+++ p32.hs
@@ -0,0 +1,14 @@
+-- Determine the greatest common divisor of two positive integer numbers. Use Euclid's algorithm
+
+import Test.HUnit.Base
+
+main :: IO ()
+main = do
+  [myGCD 36 63, myGCD (-3) (-6), myGCD (-3) 6] @?= [9,3,3]
+
+myGCD :: Int -> Int -> Int
+myGCD a 0 = a
+myGCD 0 b = b
+myGCD a b = myGCD min' (max' `mod` min')
+  where min' = if a < b then abs a else abs b
+        max' = if a < b then abs b else abs a
